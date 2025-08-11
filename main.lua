82793c77773b7541e487f025a138cce96c953f08
DjSapsan <djsapsan@gmail.com>
2025-08-11 22:20:40 +0300

    clean up IRL and time from main.lua

--- main.lua
+++ main.lua
@@ -100,7 +100,7 @@ Each day I'm loading new data
 
 function love.draw()
 
-  local numberOfGamesPrint = string.format("Number of games played: %i / %i [%i%%]", Game.stat.totalGames, Game.gamesLeftToPlay, 100 * Game.stat.totalGames/Game.gamesLeftToPlay)
+  --local numberOfGamesPrint = string.format("Number of games played: %i / %i [%i%%]", Game.stat.totalGames, Game.gamesLeftToPlay, 100 * Game.stat.totalGames/Game.gamesLeftToPlay)
   love.graphics.setColor(1,1,1,1)
   Graphics.drawCanvas(EloGraph,10,50,1,1)
   Graphics.drawCanvas(PlayersGraph,10,660,1,1)
@@ -109,16 +109,16 @@ function love.draw()
   --local topPlayer = Game.playerDB.table[1]
   --love.graphics.print("#1:"..topPlayer.name.." | ELO: "..topPlayer.rating.."| Games: "..topPlayer.games.."| Winrate: "..math.floor((topPlayer.wins / topPlayer.games) * 100).."%",0,0)
   --love.graphics.print("Number of players: "..#Game.playerDB.table,0,20)
-  love.graphics.print(numberOfGamesPrint,0,10)
+  --love.graphics.print(numberOfGamesPrint,0,10)
   love.graphics.print(info,600,20,0,2,2)
   love.graphics.print("Previous run: ",660,380)
-  love.graphics.print(parameters.lastPrediction,660,400)
+  --love.graphics.print(parameters.lastPrediction,660,400)
   --love.graphics.print("Steps: "..Game.stat.step,0,10)
   --if parameters.selected then love.graphics.print(Player.shortInfo(parameters.selected),0,80) end
   --love.graphics.print("Space - pause | Right Button Mouse - one step | 1 - add 10 new players | 2 - add 1000 | Tab - enable new players",600,10,0,1.5,1.5)
   --love.graphics.print("Sim speed: "..parameters.simSpeed,600,20)
-  love.graphics.print("Leaderboard RB EW - sorted by highest Elo:", 1600,0)
+  love.graphics.print("Leaderboard 1v1 - sorted by highest Elo:", 1600,0)
   love.graphics.print(Game.topString, 1600,20)
-  love.graphics.print(os.date("%d %B",Game.simIRLDay),1400,26,0,4,4)
+  --love.graphics.print(os.date("%d %B",Game.simIRLDay),1400,26,0,4,4)
 
 end
